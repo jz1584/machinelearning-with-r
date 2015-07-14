9e77348e4deef865ad0d7e7fb59654412cf1e121
jianming Zhou <diymyfuture@gmail.com>
2015-07-13 20:53:11 -0400

    updated code

--- practicalML.R
+++ practicalML.R
@@ -33,10 +33,43 @@ dim(testSet)
 
 #using train function in training model
 args(train.default)#show default setting for train function
+args(trainControl)
+
+set.seed(1)#set seed for consistant random samples
 Train_mod1<-train(type~.,data=trainSet,method="glm")
 Train_mod1$finalModel
 
 pred<-predict(Train_mod1,newdata=testSet)
 
 confusionMatrix(pred,testSet$type)
+
+
+
+#########
+##plotting vars example:Wage data
+rm(list = ls())
+library(ISLR)
+library(ggplot2)
+library(caret)
+library(Hmisc)
+data(Wage);str(Wage)
+
+#split by index
+indexTrain<-createDataPartition(y=Wage$wage,p=0.7,list=FALSE)
+training<-Wage[indexTrain,]
+testing<-Wage[-indexTrain,]
+
+featurePlot(x=training[,c('age','year','logwage')],y=training$wage,plot='pairs')
+#using ggplot for vivi plot
+qq<-qplot(age,logwage,colour=education,data=training)
+#geometric smoothing
+qq+ geom_smooth(method='lm',formula=y~x)
+
+#categoricalize numercial veriables: gen new var
+wage.c<-cut2(training$wage,g=3)
+
+
+
+
+
 
